8482d07b7cfc3b6252c9ef7aad814741fd0380eb
ggaim <yasir@github.com>
2022-04-30 12:34:34 +0600

    numpy implementation done.

--- clnt.py
+++ clnt.py
@@ -1,23 +1,28 @@
 from hashlib import md5
 import numpy as np
 
+def vec(t):
+    return md5(t).hexdigest()
+
 key_block_num = 0
-key_space = 20
+key_space = 22
 block_size = 2 ** key_space
 start_val = key_block_num * block_size 
 end_val = (key_block_num + 1) * block_size
 roll_T = "0421312015"
 matchstr = "00015"
 success_resp = b'SCS'
+max_key_length = len(str(2**32))
+# print(max_key_length)
 
 marray = np.arange(start_val, end_val)
 marray = marray.astype(str)
-marray = np.char.zfill(marray, 10)
+marray = np.char.zfill(marray, max_key_length)
 
 tarray = np.char.add(roll_T, marray)
 
 earray = np.char.encode(tarray)
-vec = lambda t: md5(t).hexdigest()
+# vec = lambda t: md5(t).hexdigest()
 trun = np.vectorize(vec)
 harray = trun(earray)
 sharray = harray.astype('U5')
@@ -29,3 +34,6 @@ resarray = np.char.encode(resarray)
 # print(resarray)
 finarray = np.char.add(success_resp, resarray)
 print(finarray)
+
+for pkt in finarray:
+    print(pkt)
--- clntn.py
+++ clntn.py
@@ -2,6 +2,9 @@ from hashlib import md5
 import socket
 import numpy as np
 
+def md5_calc(val):
+    return md5(val).hexdigest()
+
 host = "127.0.0.1"
 port = 54321
 server_addr = (host, port)
@@ -9,12 +12,16 @@ total_key_bits = 32
 block_addr_bits = 10
 block_key_bits = total_key_bits - block_addr_bits
 block_size = 2**block_key_bits
+max_key_digits = len(str(2**total_key_bits))
+success_resp = b'SCS'
+request_resp = b'REQ'
+next_resp = b'NXT'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect_ex(server_addr)
 
     print("Sending REQ")
-    sock.sendall(b'REQ')
+    sock.sendall(request_resp)
 
     recv_data = sock.recv(14)
     roll_T = recv_data[:10].decode()
@@ -25,14 +32,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         start_val = key_block_num * block_size 
         end_val = (key_block_num + 1) * block_size
         nonce_array = np.arange(start_val, end_val)
+        nonce_array = nonce_array.astype(str)
+        nonce_array = np.char.zfill(nonce_array, max_key_digits)
 
-        for nonce in np.arange(start_val, end_val):
-            V = roll_T + str(nonce).zfill(key_space)
-            M = md5(V.encode()).hexdigest()
-            if M[:5] == match_string_S:
-                print(M)
-                send_data = ("SCS" + M).encode()
-                sock.sendall(send_data)
+        v_array = np.char.add(roll_T, nonce_array)
+
+        v_array = np.char.encode(v_array)
+        vectorized_md5_calc = np.vectorize(md5_calc)
+        hash_array = vectorized_md5_calc(v_array)
+
+        hash_array_5dig = hash_array.astype('U5')
+        matched_pos_mask = np.char.equal(match_string_S, hash_array_5dig)
+
+        result_hash_array = hash_array[matched_pos_mask]
+
+        result_hash_array = np.char.encode(result_hash_array)
+        response_hash_packets = np.char.add(success_resp, result_hash_array)
+
+        for pkt in response_hash_packets:
+            sock.sendall(pkt)
+
+
+        # for nonce in np.arange(start_val, end_val):
+        #     V = roll_T + str(nonce).zfill(key_space)
+        #     M = md5(V.encode()).hexdigest()
+        #     if M[:5] == match_string_S:
+        #         print(M)
+        #         send_data = ("SCS" + M).encode()
+        #         sock.sendall(send_data)
 
         print("Sending NXT")
         sock.sendall(b'NXT')
